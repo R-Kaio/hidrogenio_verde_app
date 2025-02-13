09d4f3cb4d85cd05df61ee989c5727b1b2b456d5
Kaio <riancaxias@aluno.uespi.br>
2025-02-13 11:27:31 -0300

    ajustes

--- lib/auditor/verificacao_screen.dart
+++ lib/auditor/verificacao_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class VerificacaoScreen extends StatelessWidget {
+  const VerificacaoScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
